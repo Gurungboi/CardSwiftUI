601a0c981fa4b34a167f04a44b873209de9b99fd
Sunil Gurung <sunil@prezzee.com.au>
2021-05-27 22:31:00 +1000

    Initial Commit

---
+++ CardSwiftUI/CardSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardSwiftUIApp.swift
+//  CardSwiftUI
+//
+//  Created by Sunil on 27/5/21.
+//
+
+import SwiftUI
+
+@main
+struct CardSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
